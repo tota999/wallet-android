46bce453b47ac397f03d9ce05f312e36187a5033
Andrey Ilyunin <orkay255@gmail.com>
2020-10-08 19:52:29 +0300

    Add custom bridges page

--- app/src/main/java/com/tari/android/wallet/ui/activity/settings/BridgeConfigurationActivity.kt
+++ app/src/main/java/com/tari/android/wallet/ui/activity/settings/BridgeConfigurationActivity.kt
@@ -2,10 +2,13 @@ package com.tari.android.wallet.ui.activity.settings
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.tari.android.wallet.R
 import com.tari.android.wallet.ui.fragment.settings.bridge.BridgesConfigurationFragment
+import com.tari.android.wallet.ui.fragment.settings.bridge.CustomBridgesConfigurationFragment
 
-class BridgeConfigurationActivity : AppCompatActivity() {
+class BridgeConfigurationActivity : AppCompatActivity(), BridgesConfigurationFragment.Router,
+    CustomBridgesConfigurationFragment.Router {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +19,38 @@ class BridgeConfigurationActivity : AppCompatActivity() {
                 .add(R.id.fragment_container_view, BridgesConfigurationFragment.newInstance())
                 .commit()
         }
+    }
+
+    override fun back() = onBackPressed()
+
+    override fun toCustomBridgesConfiguration() =
+        addFragment(
+            supportFragmentManager.fragments.single(),
+            CustomBridgesConfigurationFragment.newInstance(),
+            allowStateLoss = true
+        )
+
+    // nyarian:
+    // allowStateLoss parameter is necessary to resolve device-specific issues like one
+    // for samsung devices with biometrics enabled, as after launching the biometric prompt
+    // onSaveInstanceState is called, and commit()ing any stuff after onSaveInstanceState is called
+    // results into IllegalStateException: Can not perform this action after onSaveInstanceState
+    @Suppress("SameParameterValue")
+    private fun addFragment(
+        sourceFragment: Fragment,
+        fragment: Fragment,
+        allowStateLoss: Boolean = false
+    ) {
+        supportFragmentManager
+            .beginTransaction()
+            .setCustomAnimations(
+                R.anim.enter_from_right, R.anim.exit_to_left,
+                R.anim.enter_from_left, R.anim.exit_to_right
+            )
+            .hide(sourceFragment)
+            .add(R.id.fragment_container_view, fragment, fragment.javaClass.simpleName)
+            .addToBackStack(fragment.javaClass.simpleName)
+            .apply { if (allowStateLoss) commitAllowingStateLoss() else commit() }
     }
 
 }
--- app/src/main/java/com/tari/android/wallet/ui/fragment/settings/bridge/CustomBridgesConfigurationFragment.kt
+++ app/src/main/java/com/tari/android/wallet/ui/fragment/settings/bridge/CustomBridgesConfigurationFragment.kt
@@ -5,38 +5,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.tari.android.wallet.databinding.FragmentBridgeConfigurationBinding
+import com.tari.android.wallet.databinding.FragmentCustomBridgesConfigurationBinding
 import com.tari.android.wallet.ui.extension.ThrottleClick
 
-class BridgesConfigurationFragment @Deprecated(
+class CustomBridgesConfigurationFragment @Deprecated(
     """Use newInstance() and supply all the 
 necessary data via arguments instead, as fragment's default no-op constructor is used by the 
 framework for UI tree rebuild on configuration changes"""
 ) constructor() : Fragment() {
 
-    private lateinit var ui: FragmentBridgeConfigurationBinding
+    private lateinit var ui: FragmentCustomBridgesConfigurationBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? =
-        FragmentBridgeConfigurationBinding.inflate(inflater, container, false).also { ui = it }.root
+    ): View? = FragmentCustomBridgesConfigurationBinding.inflate(inflater, container, false)
+        .also { ui = it }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         ui.backCtaView.setOnClickListener(ThrottleClick { (requireActivity() as Router).back() })
-        ui.customBridgesCtaView.setOnClickListener(ThrottleClick { (requireActivity() as Router).toCustomBridgesConfiguration() })
     }
 
     interface Router {
         fun back()
-        fun toCustomBridgesConfiguration()
     }
 
     companion object {
         @Suppress("DEPRECATION")
-        fun newInstance() = BridgesConfigurationFragment()
+        fun newInstance() = CustomBridgesConfigurationFragment()
     }
 
 }
