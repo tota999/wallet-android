6a09e4d9ee1b62c5cb37092a824551c917e47a34
Andrey Ilyunin <orkay255@gmail.com>
2020-10-09 18:34:09 +0300

    Introduce QR code scanning and uploading for custom bridges page

--- app/src/main/java/com/tari/android/wallet/ui/activity/settings/BridgeConfigurationActivity.kt
+++ app/src/main/java/com/tari/android/wallet/ui/activity/settings/BridgeConfigurationActivity.kt
@@ -1,9 +1,14 @@
 package com.tari.android.wallet.ui.activity.settings
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.tari.android.wallet.R
+import com.tari.android.wallet.R.string.custom_bridge_settings_pick_image
+import com.tari.android.wallet.ui.activity.qr.EXTRA_QR_DATA
+import com.tari.android.wallet.ui.activity.qr.QRScannerActivity
+import com.tari.android.wallet.ui.extension.string
 import com.tari.android.wallet.ui.fragment.settings.bridge.BridgesConfigurationFragment
 import com.tari.android.wallet.ui.fragment.settings.bridge.CustomBridgesConfigurationFragment
 
@@ -21,12 +26,39 @@ class BridgeConfigurationActivity : AppCompatActivity(), BridgesConfigurationFra
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        overridePendingTransition(R.anim.enter_from_bottom, R.anim.exit_to_top)
+    }
+
+    override fun toQRScanner() {
+        val intent = Intent(this, QRScannerActivity::class.java)
+        startActivityForResult(intent, QRScannerActivity.REQUEST_QR_SCANNER)
+        overridePendingTransition(R.anim.slide_up, 0)
+    }
+
+    override fun toImageChooser() {
+        startActivityForResult(
+            Intent.createChooser(
+                Intent().apply {
+                    type = "image/*"
+                    action = Intent.ACTION_GET_CONTENT
+                },
+                string(custom_bridge_settings_pick_image)
+            ), IMAGE_PICKER_REQUEST_CODE
+        )
+    }
+
     override fun back() = onBackPressed()
 
     override fun toCustomBridgesConfiguration() =
         addFragment(
             supportFragmentManager.fragments.single(),
-            CustomBridgesConfigurationFragment.newInstance(),
+            CustomBridgesConfigurationFragment.newInstance(
+                qrRequestCode = QRScannerActivity.REQUEST_QR_SCANNER,
+                qrDataKey = EXTRA_QR_DATA,
+                imageRequestCode = IMAGE_PICKER_REQUEST_CODE,
+            ),
             allowStateLoss = true
         )
 
@@ -51,6 +83,16 @@ class BridgeConfigurationActivity : AppCompatActivity(), BridgesConfigurationFra
             .add(R.id.fragment_container_view, fragment, fragment.javaClass.simpleName)
             .addToBackStack(fragment.javaClass.simpleName)
             .apply { if (allowStateLoss) commitAllowingStateLoss() else commit() }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        supportFragmentManager.fragments.lastOrNull()
+            ?.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private companion object {
+        private const val IMAGE_PICKER_REQUEST_CODE = 12312
     }
 
 }
--- app/src/main/java/com/tari/android/wallet/ui/fragment/settings/bridge/CustomBridgesConfigurationFragment.kt
+++ app/src/main/java/com/tari/android/wallet/ui/fragment/settings/bridge/CustomBridgesConfigurationFragment.kt
@@ -1,12 +1,27 @@
 package com.tari.android.wallet.ui.fragment.settings.bridge
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.google.zxing.BinaryBitmap
+import com.google.zxing.MultiFormatReader
+import com.google.zxing.RGBLuminanceSource
+import com.google.zxing.common.HybridBinarizer
+import com.orhanobut.logger.Logger
 import com.tari.android.wallet.databinding.FragmentCustomBridgesConfigurationBinding
 import com.tari.android.wallet.ui.extension.ThrottleClick
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+
 
 class CustomBridgesConfigurationFragment @Deprecated(
     """Use newInstance() and supply all the 
@@ -15,6 +30,8 @@ framework for UI tree rebuild on configuration changes"""
 ) constructor() : Fragment() {
 
     private lateinit var ui: FragmentCustomBridgesConfigurationBinding
+
+    private val router get() = requireActivity() as Router
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,16 +42,78 @@ framework for UI tree rebuild on configuration changes"""
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        ui.backCtaView.setOnClickListener(ThrottleClick { (requireActivity() as Router).back() })
+        ui.backCtaView.setOnClickListener(ThrottleClick { router.back() })
+        ui.scanBridgesQrCtaView.setOnClickListener(ThrottleClick {
+            afterDelay { router.toQRScanner() }
+        })
+        ui.uploadBridgesQrCtaView.setOnClickListener(ThrottleClick {
+            afterDelay { router.toImageChooser() }
+        })
+    }
+
+    private fun afterDelay(action: () -> Unit) {
+        lifecycleScope.launchWhenResumed {
+            delay(100L)
+            withContext(Dispatchers.Main) { action() }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            val arguments = arguments!!
+            when (requestCode) {
+                arguments.getInt(QR_REQUEST_CODE_KEY) ->
+                    data.getStringExtra(arguments.getString(QR_DATA_KEY_KEY))
+                        ?.let(::parseBridgesFromQRCodeResult)
+                arguments.getInt(IMAGE_REQUEST_CODE_KEY) ->
+                    extractQRCodeContentFromImage(data.data!!)?.let(::parseBridgesFromQRCodeResult)
+            }
+        }
+    }
+
+    private fun extractQRCodeContentFromImage(data: Uri): String? {
+        val inputStream = requireContext().contentResolver.openInputStream(data)
+        val bitmap = BitmapFactory.decodeStream(inputStream)
+        val intArray = IntArray(bitmap.width * bitmap.height)
+        bitmap.getPixels(intArray, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+        val binaryBitmap =
+            BinaryBitmap(HybridBinarizer(RGBLuminanceSource(bitmap.width, bitmap.height, intArray)))
+        return try {
+            MultiFormatReader().decode(binaryBitmap).text
+        } catch (e: Exception) {
+            Logger.e(e, "Error decoding barcode")
+            null
+        }
+    }
+
+    private fun parseBridgesFromQRCodeResult(result: String) {
+        Toast.makeText(requireContext(), result, Toast.LENGTH_LONG).show()
     }
 
     interface Router {
+        fun toQRScanner()
+        fun toImageChooser()
         fun back()
     }
 
     companion object {
+        private const val QR_REQUEST_CODE_KEY = "qr_request_code"
+        private const val QR_DATA_KEY_KEY = "qr_data_key"
+        private const val IMAGE_REQUEST_CODE_KEY = "image_request_code"
+
         @Suppress("DEPRECATION")
-        fun newInstance() = CustomBridgesConfigurationFragment()
+        fun newInstance(
+            qrRequestCode: Int,
+            qrDataKey: String,
+            imageRequestCode: Int,
+        ) =
+            CustomBridgesConfigurationFragment().also {
+                it.arguments = Bundle().apply {
+                    putInt(QR_REQUEST_CODE_KEY, qrRequestCode)
+                    putString(QR_DATA_KEY_KEY, qrDataKey)
+                    putInt(IMAGE_REQUEST_CODE_KEY, imageRequestCode)
+                }
+            }
     }
 
 }
